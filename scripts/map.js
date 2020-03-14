085a79c1676c0e65c805798646886a7345a8ae11
Belonozhkin <igor.belonozhkin@yandex.by>
2020-03-14 09:51:43 +0300

    map

--- scripts/map.js
+++ scripts/map.js
@@ -1,13 +1,12 @@
 function initMap() {
-
-    var map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 53.908978, lng: 27.522993},
+	var map = new google.maps.Map(document.getElementById('map'), {
+		center: { lat: 53.908978, lng: 27.522993 },
 		zoom: 16
 	});
 
 	var marker = new google.maps.Marker({
-	 	position: {lat: 53.908978, lng: 27.522993}, 
-	 	map: map
+		position: { lat: 53.908978, lng: 27.522993 },
+		map: map
 	});
 }
 
